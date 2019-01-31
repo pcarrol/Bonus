80707f00662569a9e7db28559e7afd567952bc7e
pcarrol <31235138+pcarrol@users.noreply.github.com>
2019-01-31 20:39:28 +1000

    Add files via upload

---
+++ Muck around.py
@@ -0,0 +1,45 @@
+##Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
+#name = input("Enter your name: ")
+#print(name)
+#def getuserage() -> int:
+#    while True:
+#        try:
+#            return int(input("Enter your age in years: "))
+#        except ValueError:
+#            print("Age must be a positive whole number. ")
+#import datetime
+#curryear = datetime.datetime.today().year
+#birthyear = curryear - age
+#year100 = birthyear + 100
+#print(name, "you will turn 100 in the year ", year100)
+
+#def calcage():
+#    dob = input("Enter your date of birth: ")
+#    age = dateutil(dob)
+
+#print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are")
+
+#import datetime
+#currdatetime = datetime.datetime.now()
+#print("Current date and time: \n",currdatetime.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+#get circle radius and compute area
+#def compute_circle_area(radius):
+#    radius = float(radius)
+#    import math
+#    pi = math.pi
+#    area = pi*(radius**2.0)
+#    return area
+
+#radius = input("Enter the radius with a decimal point: ")
+#print("The area is: ", compute_circle_area(radius))
+
+a = list(range(1,100))
+mof3or5 = []
+for i in a:
+    if i % 3 == 0 or i % 5 == 0:
+        mof3or5.append(i)
+print("The multiples of 3 and 5 are: ", mof3or5)
+
+
